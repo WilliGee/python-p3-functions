ad2263f98c8b058e29f683ba3ddb90887df99929
Wilson Gachanja <wilson.gachanja@student.moringaschool.com>
2024-01-19 22:35:45 +0300

    Initial Commit

--- lib/functions.py
+++ lib/functions.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(Guido):
+    print("Hello, Guido!")
     pass
 
 def greet_with_default(name="programmer"):
+    print("Hello," + name)
     pass
 
-def add(num1, num2):
+def add(num1=45, num2=55):
+    sum = num1 + num2
+    print("Sum:", sum)
     pass
 
 def halve(number):
